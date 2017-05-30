0dfd42a24a168bda219a68a507342865ba25f3d3
SEFAZ\02890117103 <charlesbritoneresjr@gmail.com>
2017-05-30 14:50:49 -0300

    fist commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// var itens = $(".itens-total");
+// for (var i=0; i<itens.length; i++){
+//   var item = $(itens[i]);
+//   var valor = item.text();
+// }
+
+function soma(a, b){
+  return a+b;
+}
